a3297bbbf9536c1a687957621fb6fdf07fad6523
dheera123 <dheerajemoz@gmail.com>
2023-08-17 14:00:00 +0530

    second commit

--- lib/Firebase_conn.dart
+++ lib/Firebase_conn.dart
@@ -54,6 +54,7 @@ class _FireConnectState extends State<FireConnect> {
                 ),
                 ),
               ),
+
               FloatingActionButton(onPressed: (){
               sentData();
               }),
